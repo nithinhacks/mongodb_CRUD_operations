2900872ea218de842b84af3184f81a8205baf566
nithinhacks <nithinbhpv@gmail.com>
2021-09-28 19:53:37 +0530

    first commit

---
+++ test/test_helper.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+before((done) => {
+    mongoose.connect('mongodb://localhost/users_test');
+    mongoose.connection
+        .once('open', () => { done(); })
+        .on('error', (error) => {
+            console.warn('warning', error);
+        });
+});
+
+
+beforeEach((done) => {
+    mongoose.connection.collections.users.drop(() => {
+        done();
+    });
+});
